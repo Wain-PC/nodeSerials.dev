a77913af03e6d4abca335ca36c911b33f2d564a9
Wain <wain-pc@mail.ru>
2014-02-18 16:48:25 +0400

    Further sketches on TheTVDB parser.

---
+++ app/external/thetvdb/api.js
@@ -0,0 +1,90 @@
+var base_uri = "http://thetvdb.com/api/",
+    language = "en",
+    request = require("../../util/request"),
+    parser = require("xml2json"),
+    userAgent = 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14'; ////@TODO: move this to config
+
+request = new request("", 'GET');
+
+module.exports = function (access_token) {
+
+    var resources = {};
+
+    function parsereq(url, cb) {
+        request.makeRequest(url, false, function (error, response, body) {
+            if (response.statusCode === 200) {
+                jsonbody = parser.toJson(body, {object: true, sanitize: false});
+                if (jsonbody.Error) {
+                    error = jsonbody.Error;
+                    jsonbody = null;
+                }
+                cb(error, jsonbody);
+            }
+            else {
+                cb(error ? error : "Could not complete the request", null);
+            }
+        });
+    }
+
+
+    //  Languages
+
+    resources.getLanguages = function (cb) {
+        var url = base_uri + access_token + "/languages.xml";
+        parsereq(url, cb);
+    };
+
+    resources.getLanguage = function () {
+        return language;
+    };
+
+    resources.setLanguage = function (lang) {
+        language = lang;
+    };
+
+
+    //  Time
+
+    resources.getTime = function (cb) {
+        var url = base_uri + "Updates.php?type=none";
+        parsereq(url, cb);
+    };
+
+
+    //  Series
+
+    resources.getSeries = function (name, cb) {
+        var url = base_uri + "GetSeries.php?seriesname=" + name + "&language=" + language;
+        parsereq(url, cb);
+    };
+
+    resources.getSeriesById = function (id, cb) {
+        var url = base_uri + access_token + "/series/" + id + "/" + language + ".xml";
+        parsereq(url, cb);
+    };
+
+    resources.getFullSeriesInfoById = function (id, cb) {
+        var url = base_uri + access_token + "/series/" + id + "/all/" + language + ".xml";
+        parsereq(url, cb);
+    };
+
+    resources.getActors = function (id, cb) {
+        var url = base_uri + access_token + "/series/" + id + "/actors.xml";
+        parsereq(url, cb);
+    };
+
+    resources.getBanners = function (id, cb) {
+        var url = base_uri + access_token + "/series/" + id + "/banners.xml";
+        parsereq(url, cb);
+    };
+
+
+    //  Updates
+
+    resources.getUpdates = function (time, cb) {
+        var url = base_uri + "Updates.php?type=all&time=" + time;
+        parsereq(url, cb);
+    };
+
+    return resources;
+};
--- app/external/thetvdb/index.js
+++ app/external/thetvdb/index.js
@@ -1,7 +1,64 @@
-/**
- * Created with JetBrains WebStorm.
- * User: wain-_000
- * Date: 18.02.14
- * Time: 0:47
- * To change this template use File | Settings | File Templates.
- */
+function TheTvDbParser(key) {
+    this.key = key;
+    this.api = require("./api.js");
+    this.show = {};
+
+
+    this.show.searchForShow = function (series, callback) {
+        var _this = this;
+        var key = this.key;
+        var showTitle,
+            searchLang = 'ru'; //default value
+        if (!series) {
+            console.log("Series name is empty, aborting!");
+            callback(false);
+            return;
+        }
+
+        if (series.title_en) {
+            console.log("Got en title!");
+            showTitle = series.title_en;
+        }
+        else if (series.title_ru) {
+            console.log("Got ru title!");
+            showTitle = series.title_ru;
+            searchLang = 'ru';
+        }
+
+        else {
+            console.log("Sorry, cannot parse empty title! Aborting!");
+            callback(false);
+            return;
+        }
+
+        //set language for appropriate language
+        this.api(key).setLanguage(searchLang);
+        console.log("Set language " + searchLang);
+
+        this.api(key).getSeries(showTitle, function (err, res) {
+            if (!err) {
+
+                if (res.Data.Series.length) {
+                    //@TODO: select needed series from the list
+                    var seriesId = res.Data.Series[0].seriesid;
+
+                    //get episodes list from the chosen series
+                    _this.api(key).getFullSeriesInfoById(seriesId, function (err, res) {
+                        console.log("Full res:" + JSON.stringify(res));
+                        callback(res);
+                        return true;
+                    });
+                    console.log("Got info from theTVDB successfully:" + JSON.stringify(res));
+                    //callback(res);
+                    return true;
+                }
+            }
+
+            console.log(err);
+            callback(false);
+        });
+    }.bind(this);
+
+}
+
+module.exports = TheTvDbParser;

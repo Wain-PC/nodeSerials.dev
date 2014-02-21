718ba993c1878b1b61c590532b33cc8f5f401447
Wain <wain-pc@mail.ru>
2014-02-21 22:52:13 +0400

    More functionality to the module. Possible fix of 'object' method.

--- app/util/is/index.js
+++ app/util/is/index.js
@@ -1,14 +1,23 @@
-var Is =  {
-    number : function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+var Is = {
+    number: function (n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
     },
 
-array : function (n) {
-    if (Object.prototype.toString.call(n) === '[object Array]') {
-        return true;
+    array: function (n) {
+        return (Object.prototype.toString.call(n) === '[object Array]');
+    },
+
+    string: function (n) {
+        return (typeof n == 'string' || n instanceof String);
+    },
+
+    object: function (n) {
+        return (Object.prototype.toString.call(n) === '[object Object]');
+    },
+
+    function: function (n) {
+        return (typeof(n) === 'function');
     }
-    return false;
-}
 };
 
 module.exports = Is;

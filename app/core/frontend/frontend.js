90dbfb77c478200e2619f73f665168ba41fcb1cc
Wain <wain-pc@mail.ru>
2014-02-28 01:54:54 +0400

    Implemented more frontend methods and some Jade templates to view results.

--- app/core/frontend/frontend.js
+++ app/core/frontend/frontend.js
@@ -18,24 +18,55 @@ module.exports = function (app) {
         var Series = this.getModels().Series;
         var res = [];
         Series.findAndCountAll({
+            order: 'id DESC',
             limit: 10
         }).success(function (result) {
-                console.log(result.rows[0].values);
-                for (var i = 0; i < result.rows.length; i++) {
-                    res.push({
-                        title: result.rows[i].values.title_ru,
-                        url: '#'
-                    });
-                }
-                callback(res);
+                callback(result.rows);
+            });
+    };
+
+    Frontend.prototype.getSeriesById = function (id, callback) {
+        var Series = this.getModels().Series;
+        var Season = this.getModels().Season;
+        var Episode = this.getModels().Episode;
+        var Video = this.getModels().Video;
+
+
+        Series.find({
+            where: {
+                id: id
+            },
+            include: [
+                {model: Season, as: 'Seasons',
+                    include: [
+                        {model: Episode, as: 'Episodes',
+                            include: [
+                                {model: Video, as: 'Videos'}
+                            ]}
+                    ]}
+            ]
+        }).success(function (series) {
+                console.log(series.seasons[0].episodes[0].values);
+                callback(series.values);
             });
     }
 
     app.get("/frontend/latest", function (request, response) {
         var f = new Frontend(app);
         f.getLatestSeries(10, function (res) {
-            response.render('genreList',
+            response.render('seriesList',
                 { dataArray: res, rawData: JSON.stringify(res)}
+            );
+        });
+    });
+
+    app.get("/frontend/series/:id", function (request, response) {
+        var f = new Frontend(app);
+        var id = request.params.id;
+        if (!id) return false;
+        f.getSeriesById(request.params.id, function (res) {
+            response.render('series',
+                { series: res }
             );
         });
     });
